7eddfc3e2a738df188e3f9222352d0e735f9191d
vitalyionov <vitalyionov@gmail.com>
2025-09-26 17:14:24 +0200

    добавляет возможность разблокировать уровни

--- app/api/routes/client/levels.py
+++ app/api/routes/client/levels.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, Query
 
 from app.models import User
+from app.schemas.admin import CategoryProgressInfoOut
 from app.schemas.client import LevelsListResponse
 from app.core.dependencies.common import get_current_user
 from app.core.dependencies.client import get_level_service
+from app.core.dependencies.admin import get_category_progress_info_service
 from app.services.client import LevelService
+from app.services.admin import CategoryProgressInfoService
 
 router = APIRouter(tags=["levels"])
 
@@ -16,3 +19,15 @@ async def read_levels(
     current_user: User = Depends(get_current_user),
 ):
     return await svc.get_all(user_id=current_user.id, category_id=category_id)
+
+
+@router.post("/levels/unlock", response_model=CategoryProgressInfoOut)
+async def unlock_level(
+    category_id: int,
+    level_id: int,
+    svc_cpi: CategoryProgressInfoService = Depends(get_category_progress_info_service),
+    current_user: User = Depends(get_current_user),
+):
+    return await svc_cpi.get_or_create(
+        user_id=current_user.id, category_id=category_id, level_id=level_id
+    )
